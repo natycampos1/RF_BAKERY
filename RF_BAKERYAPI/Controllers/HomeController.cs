854be32bee416a88e0df38aadb0780de7e13e160
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-06-19 21:32:14 -0600

    Inicio de proyecto
    Semana 1

---
+++ RF_BAKERYAPI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RF_BAKERYAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+   
+    }
+}
+
